52d97924e4790395c6d7e2ffd40a09729f511a89
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-01-18 04:35:05 +0000

    1

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,37 @@
 print("Hello from lesson 1")
+#check if item is paper, plastic, glass
+#if item is paper put it in the paper bin
+#if item is plastic, put it in the plastic bin
+#if item is glass put it in the glass bin
+red = 1
+blue = 2
+green = 3
+tred = red * 3
+tblue = blue * 5
+tgreen = green * 4
+total = tred + tblue + tgreen
+print(total)
 
+score_one = 80
+score_two = 90
+score_three = 75
+
+total = score_one + score_two + score_three
+
+average_score = total / 3
+
+student_name = "Alex"
+
+print("Average score for " , student_name + " is: " , average_score)
+
+
+ans = int(input("enter score: "))
+
+if ans > 74 :
+    print("Grade A")
+elif ans > 59 and ans < 75 :
+    print("Grade B")
+elif ans > 49 and ans < 60 :
+    print("Grade C")
+else :
+    print("Fail")
--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,75 @@
-print("Hello from lesson 2")
+#print("Hello from lesson 2")
 
+
+#for char in range(21) :
+ #   print (char)
+
+#for hi in range(1,31,1) :
+ #   print (hi)
+
+#for bye in range(2,25,1) :
+ #   print (bye)
+
+#abc = 0
+
+#while abc < 21 :
+#    print (abc)
+#    abc = abc + 1
+
+#bcd = 1
+
+#while bcd < 31 :
+#    print (bcd)
+#    bcd = bcd + 1
+
+#ad = 2
+
+#while ad < 25 :
+#    print (ad)
+#    ad = ad + 1
+
+
+#hi = 1
+
+#while hi < 11:
+#    print (hi)
+#    hi = hi + 1
+#    if hi == 5 :
+#        break
+
+#a = 1
+
+#while a < 6:
+#    if a == 5:
+#        break
+#    else:
+#        print (a)
+#        a += 1
+
+toppingtotal = " "
+
+#while True:
+#    topping = input("what topping do you want? ")
+#    if topping == "end":
+#        print(toppingtotal)
+#        break
+#    else:
+#        toppingtotal = topping , "," , toppingtotal
+
+hi = True
+
+while hi == True:
+    q1 = input("what is the capital of singapore?")
+    if q1 == "singapore":
+        q2 = input("what is singapore's most iconic seafood?")
+        if q2 == "chili crab":
+            q3 = input("what is 1 + 1?")   
+            if q3 == "2":
+                 break
+            else :
+                hi = True
+        else :
+            hi = True
+    else :
+        hi = True
+        
